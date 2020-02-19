bcb30674fbc4ec3d1d2b594e9404f118b9965c61
Veronica <green.veronica@gmail.com>
2020-02-19 09:00:38 -0300

    Modificacion Vistas

--- SistemaWebCF/Models/Categoria.cs
+++ SistemaWebCF/Models/Categoria.cs
@@ -10,14 +10,16 @@ namespace SistemaWebCF.Models
 
     {
         public int idcategoria { get;set; }
+        [Display(Name ="Nombre")]
         [Required(ErrorMessage = "Ingrese un nombre.")]
         [StringLength(50, MinimumLength = 3,
     ErrorMessage = "El nombre no debe de tener más de 50 caracteres, ni menos de 3 caracteres.")]
         public string nombre { get; set; }
+        [Display(Name = "Descripcion")]
         [StringLength(255,
     ErrorMessage = "la descripción no debe de tener más de 255 caracteres.")]
         public string descripcion { get; set; }
-
+        [Display(Name = "Estado")]
         public bool? estado { get; set; }
         //una categoria puede tener mas de un producto
         public virtual ICollection<Producto> productos { get; set; }
